b964ae1022c55064dd3e6543e6ca3b29306150cc
Slobodan Kovrlija <slobkov02@yahoo.com>
2017-10-27 23:59:24 -0400

    Add custom table view cell

--- SchoolDemo/SchoolDemo/Controllers/ForecastViewController.swift
+++ SchoolDemo/SchoolDemo/Controllers/ForecastViewController.swift
@@ -8,15 +8,17 @@
 
 import UIKit
 
-class ForecastViewController: UIViewController {
+class ForecastViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     lazy var forecastService: ForecastService = ForecastService()
+    var arrayOfDailyForecasts = [DailyForecast?]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        forecastService.retrieveNYCForecast { (arrayOfDailyForecasts) in
+        forecastService.retrieveNYCForecast {[unowned self] (arrayOfDailyForecasts) in
             print(arrayOfDailyForecasts)
+            self.arrayOfDailyForecasts = arrayOfDailyForecasts
         }
         
         
@@ -27,6 +29,16 @@ class ForecastViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayOfDailyForecasts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let forecastCell: ForecastTableViewCell = tableView.dequeueReusableCell(withIdentifier: "forecastTableViewCell", for: indexPath) as! ForecastTableViewCell
+        if let dailyForecast = arrayOfDailyForecasts[indexPath.row] {
+            forecastCell.dailyForecast = dailyForecast
+        }
+        return forecastCell
+    }
 }
 
---
+++ SchoolDemo/SchoolDemo/Views/ForecastTableViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  ForecastTableViewCell.swift
+//  SchoolDemo
+//
+//  Created by Slobodan Kovrlija on 10/27/17.
+//  Copyright © 2017 Slobodan Kovrlija. All rights reserved.
+//
+
+import UIKit
+
+class ForecastTableViewCell: UITableViewCell {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var minTempLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    
+    // MARK: - Properties
+    var dailyForecast: DailyForecast? {
+        
+        didSet {
+            if let dateTimeISO = dailyForecast?.dateTimeISO {
+                dateLabel.text = dateTimeISO
+            }
+            if let maxTempF = dailyForecast?.maxTempF {
+                maxTempLabel.text = String(maxTempF)
+            }
+            if let minTempF = dailyForecast?.minTempF {
+                minTempLabel.text = String(minTempF)
+            }
+            if let icon = dailyForecast?.icon {
+                iconImageView.image = UIImage.init(named: icon)
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
